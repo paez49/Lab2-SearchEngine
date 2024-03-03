bf9847a3eef3931d8b0f3dbf7941efb3d6af7a81
Juan Páez <jdpluc302@gmail.com>
2024-03-02 23:14:58 -0500

    update crawler

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # Motor de busqueda usando indexación
 - Anderson Alvarado
 - Juan Páez
+## Obteniendo palabras que no son relevantes para el índice
+Para obtener esta lista de palabras se realizó un muestreo de los cursos, con el fin de 
+obtener la información de estos, y saber cuáles son las palabras que no son relevantes 
+para el índice.
+
+Utilizando una calculadora online para obtener el tamaño de la muestra,
+nos da este resultado:
+![Resultado tamaño muestra](images/image.png)
+Por lo tanto se ejecutó el código obteniendo **114** cursos con su respectiva información.
+
+```python
+def most_frecuent_words(words):
+```
+Esta función que se encuentra en utils.py en la clase UtilText, cumple la
+función de generar un diccionario de palabras con su respectiva frecuencia.
+Y a partir del resultado, se realizará un análisis de que palabras deberían omitirse
+en la creación del índice.
+
+A continuación, se presenta, parcialmente, el resultado al ejecutar la obtención
+de estas palabras:
+```json
+{
+  "aplica": 1756,
+  "para": 1222,
+  "como": 582,
+  "universidad": 499,
+  "salud": 389,
+  "objetivos": 345,
+  "curso": 282,
+  "desarrollo": 267,
+  "javeriana": 263,
+  "seguridad": 253,
+  "pontificia": 233,
+  "herramientas": 229,
+  "sobre": 209,
+  "este": 207,
+  "programa": 197,
+  "trabajo": 191,
+  "aprendizaje": 187,
+  "unidad": 184,
+  "proyectos": 183,
+  "digital": 179,
+  ...
+}
+```
+A criterio de los autores de este taller, las palabras que se
+repiten desde 100 veces en adelante, no genera valor al 
+indexador a construir. Por lo tanto, seran omitidas.
--- crawler.py
+++ crawler.py
@@ -1,13 +1,10 @@
-from queue import Queue
 from typing import Dict
 import json
-import csv
 from bs4 import BeautifulSoup
-from utils import UtilURL, UtilRequests
-import requests
+from utils import UtilURL, UtilRequests, UtilText
 import pandas as pd
-import nltk
-from nltk.corpus import stopwords
+import re
+
 
 class Crawler:
     def __init__(self, start_url: str, domain: str):
@@ -43,43 +40,45 @@ class Crawler:
         """Crawl the web and build the index."""
         courses = self.get_links(self.start_url, n)
 
-        nltk.download("punkt")
-        nltk.download("stopwords")
-
+        util_text = UtilText()
+        all_words = []
         columns = ["course_id", "word"]
         df = pd.DataFrame(columns=columns)
-        try:
-            for course_id, url_course in courses.items():
-                res = self.util_requests.get_response(url_course)
 
-                soup = BeautifulSoup(res.content, "html.parser")
+        for course_id, url_course in courses.items():
+            res = self.util_requests.get_response(url_course)
+
+            soup = BeautifulSoup(res.content, "html.parser")
+
+            title_wrapper = soup.find("div", class_="subtitle")
+
+            course_wrapper = soup.find(
+                "div",
+                class_="course-wrapper-content col-12 col-md-8",  # Tag with all text of the course
+            )
+            if not course_wrapper:
                 course_wrapper = soup.find(
-                    "div", class_="course-wrapper-content col-12 col-md-8"
+                    "div",
+                    class_="course-wrapper-content course-wrapper-content---top col-12 col-md-8",
                 )
+            text = course_wrapper.get_text()
+            text = text + title_wrapper.get_text()
 
-                words = nltk.tokenize.word_tokenize(course_wrapper.get_text())
-                useful_words = [
-                    word.lower()
-                    for word in [
-                        word.lower()
-                        for word in words
-                        if word.isalnum()
-                        and word.lower() not in stopwords.words("spanish")
-                    ]
-                    if word.isalnum() and word.lower() not in stopwords.words("spanish")
-                ]
-                useful_words = list(set(useful_words))  # Drop duplicated
-
-                for word in useful_words:
+            useful_words = util_text.get_useful_words(text)
+            useless_words = util_text.get_useless_words()
+            
+            useful_words = list(set(useful_words))  # Drop duplicated
+            for word in useful_words:
+                if not word in useless_words:
                     df = pd.concat(
                         [df, pd.DataFrame({"course_id": [course_id], "word": [word]})],
                         ignore_index=True,
                     )
+                    
+            # all_words = all_words + useful_words
+        # util_text.most_frequent_words(all_words)
 
-        except Exception as e:
-            print(f"Error processing {self.start_url}: {e}")
-
-        with open(json_file_name, "w") as file:
+        with open(f"files/{json_file_name}", "w") as file:
             json.dump(courses, file, indent=2)
 
-        df.to_csv(output_file_name, index=False)
+        df.to_csv(f"files/{output_file_name}", index=False)
